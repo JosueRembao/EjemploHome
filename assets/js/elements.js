f80b5bf960e18b67d2c99c5d00517c9c69fec0b7
Josue Garcia <josue.garcia.rembao@gmail.com>
2017-08-02 18:38:54 -0600

    Add files via upload

---
+++ assets/js/elements.js
@@ -0,0 +1,271 @@
+        //Animaciones para cada uno de los elementos de la pagina 
+        $(window).scroll(function() {
+            $('#adelita').each(function() {
+                var imagePos = $(this).offset().top;
+                var topOfWindow = $(window).scrollTop();
+                if (imagePos < topOfWindow + 400) {
+                    $(this).addClass("stretchLeft");
+                }
+            });
+        });
+        $(window).scroll(function() {
+            $('#hidalgo').each(function() {
+                var imagePos = $(this).offset().top;
+                var topOfWindow = $(window).scrollTop();
+                if (imagePos < topOfWindow + 400) {
+                    $(this).addClass("stretchRight");
+                }
+            });
+        });
+        $(window).scroll(function() {
+            $('#servicios').each(function() {
+                var imagePos = $(this).offset().top;
+                var topOfWindow = $(window).scrollTop();
+                if (imagePos < topOfWindow + 400) {
+                    $(this).addClass("fadeIn");
+                }
+            });
+        });
+        $(window).scroll(function() {
+            $('#paginas-web').each(function() {
+                var imagePos = $(this).offset().top;
+                var topOfWindow = $(window).scrollTop();
+                if (imagePos < topOfWindow + 400) {
+                    $(this).addClass("stretchLeft");
+                }
+            });
+        });
+        $(window).scroll(function() {
+            $('#computo').each(function() {
+                var imagePos = $(this).offset().top;
+                var topOfWindow = $(window).scrollTop();
+                if (imagePos < topOfWindow + 400) {
+                    $(this).addClass("stretchRight");
+                }
+            });
+        });
+        $(window).scroll(function() {
+            $('#equipo-titulo').each(function() {
+                var imagePos = $(this).offset().top;
+                var topOfWindow = $(window).scrollTop();
+                if (imagePos < topOfWindow + 400) {
+                    $(this).addClass("fadeIn");
+                }
+            });
+        });
+        $(window).scroll(function() {
+            $('#ofertas').each(function() {
+                var imagePos = $(this).offset().top;
+                var topOfWindow = $(window).scrollTop();
+                if (imagePos < topOfWindow + 400) {
+                    $(this).addClass("pullUp");
+                }
+            });
+        });
+        $(window).scroll(function() {
+            $('#ventajas').each(function() {
+                var imagePos = $(this).offset().top;
+                var topOfWindow = $(window).scrollTop();
+                if (imagePos < topOfWindow + 400) {
+                    $(this).addClass("fadeIn");
+                }
+            });
+        });
+        $(window).scroll(function() {
+            $('#ventajas-img').each(function() {
+                var imagePos = $(this).offset().top;
+                var topOfWindow = $(window).scrollTop();
+                if (imagePos < topOfWindow + 400) {
+                    $(this).addClass("expandUp");
+                }
+            });
+        });
+        $(window).scroll(function() {
+            $('#ventajas-img2').each(function() {
+                var imagePos = $(this).offset().top;
+                var topOfWindow = $(window).scrollTop();
+                if (imagePos < topOfWindow + 400) {
+                    $(this).addClass("expandUp");
+                }
+            });
+        });
+        $(window).scroll(function() {
+            $('#ventajas-img3').each(function() {
+                var imagePos = $(this).offset().top;
+                var topOfWindow = $(window).scrollTop();
+                if (imagePos < topOfWindow + 400) {
+                    $(this).addClass("expandUp");
+                }
+            });
+        });
+        $(window).scroll(function() {
+            $('#ventajas-img4').each(function() {
+                var imagePos = $(this).offset().top;
+                var topOfWindow = $(window).scrollTop();
+                if (imagePos < topOfWindow + 400) {
+                    $(this).addClass("expandUp");
+                }
+            });
+        });
+        $(window).scroll(function() {
+            $('#ventajas-img5').each(function() {
+                var imagePos = $(this).offset().top;
+                var topOfWindow = $(window).scrollTop();
+                if (imagePos < topOfWindow + 400) {
+                    $(this).addClass("expandUp");
+                }
+            });
+        });
+        $(window).scroll(function() {
+            $('#ventajas-img6').each(function() {
+                var imagePos = $(this).offset().top;
+                var topOfWindow = $(window).scrollTop();
+                if (imagePos < topOfWindow + 400) {
+                    $(this).addClass("expandUp");
+                }
+            });
+        });
+        $(window).scroll(function() {
+            $('#Esquema').each(function() {
+                var imagePos = $(this).offset().top;
+                var topOfWindow = $(window).scrollTop();
+                if (imagePos < topOfWindow + 400) {
+                    $(this).addClass("fadeIn");
+                }
+            });
+        });
+        $(window).scroll(function() {
+            $('#elem1').each(function() {
+                var imagePos = $(this).offset().top;
+                var topOfWindow = $(window).scrollTop();
+                if (imagePos < topOfWindow + 400) {
+                    $(this).addClass("slideLeft");
+                }
+            });
+        });
+        $(window).scroll(function() {
+            $('#elem2').each(function() {
+                var imagePos = $(this).offset().top;
+                var topOfWindow = $(window).scrollTop();
+                if (imagePos < topOfWindow + 400) {
+                    $(this).addClass("slideRight");
+                }
+            });
+        });
+        $(window).scroll(function() {
+            $('#elem3').each(function() {
+                var imagePos = $(this).offset().top;
+                var topOfWindow = $(window).scrollTop();
+                if (imagePos < topOfWindow + 400) {
+                    $(this).addClass("slideLeft");
+                }
+            });
+        });
+        $(window).scroll(function() {
+            $('#elem4').each(function() {
+                var imagePos = $(this).offset().top;
+                var topOfWindow = $(window).scrollTop();
+                if (imagePos < topOfWindow + 400) {
+                    $(this).addClass("slideRight");
+                }
+            });
+        });
+        $(window).scroll(function() {
+            $('#elem5').each(function() {
+                var imagePos = $(this).offset().top;
+                var topOfWindow = $(window).scrollTop();
+                if (imagePos < topOfWindow + 400) {
+                    $(this).addClass("slideLeft");
+                }
+            });
+        });
+        $(window).scroll(function() {
+            $('#elem6').each(function() {
+                var imagePos = $(this).offset().top;
+                var topOfWindow = $(window).scrollTop();
+                if (imagePos < topOfWindow + 400) {
+                    $(this).addClass("slideRight");
+                }
+            });
+        });
+        $(window).scroll(function() {
+            $('#elem7').each(function() {
+                var imagePos = $(this).offset().top;
+                var topOfWindow = $(window).scrollTop();
+                if (imagePos < topOfWindow + 400) {
+                    $(this).addClass("slideLeft");
+                }
+            });
+        });
+        $(window).scroll(function() {
+            $('#elem8').each(function() {
+                var imagePos = $(this).offset().top;
+                var topOfWindow = $(window).scrollTop();
+                if (imagePos < topOfWindow + 400) {
+                    $(this).addClass("slideRight");
+                }
+            });
+        });
+        $(window).scroll(function() {
+            $('#elem9').each(function() {
+                var imagePos = $(this).offset().top;
+                var topOfWindow = $(window).scrollTop();
+                if (imagePos < topOfWindow + 400) {
+                    $(this).addClass("slideLeft");
+                }
+            });
+        });
+        $(window).scroll(function() {
+            $('#elem10').each(function() {
+                var imagePos = $(this).offset().top;
+                var topOfWindow = $(window).scrollTop();
+                if (imagePos < topOfWindow + 400) {
+                    $(this).addClass("slideRight");
+                }
+            });
+        });
+        $(window).scroll(function() {
+            $('#elem11').each(function() {
+                var imagePos = $(this).offset().top;
+                var topOfWindow = $(window).scrollTop();
+                if (imagePos < topOfWindow + 400) {
+                    $(this).addClass("slideLeft");
+                }
+            });
+        });
+        $(window).scroll(function() {
+            $('#equipo').each(function() {
+                var imagePos = $(this).offset().top;
+                var topOfWindow = $(window).scrollTop();
+                if (imagePos < topOfWindow + 400) {
+                    $(this).addClass("fadeIn");
+                }
+            });
+        });
+        $(window).scroll(function() {
+            $('#all-team').each(function() {
+                var imagePos = $(this).offset().top;
+                var topOfWindow = $(window).scrollTop();
+                if (imagePos < topOfWindow + 400) {
+                    $(this).addClass("expandOpen");
+                }
+            });
+        });
+        $(window).scroll(function() {
+            $('#clientes').each(function() {
+                var imagePos = $(this).offset().top;
+                var topOfWindow = $(window).scrollTop();
+                if (imagePos < topOfWindow + 400) {
+                    $(this).addClass("fadeIn");
+                }
+            });
+        });
+        $(window).scroll(function() {
+            $('#customers').each(function() {
+                var imagePos = $(this).offset().top;
+                var topOfWindow = $(window).scrollTop();
+                if (imagePos < topOfWindow + 400) {
+                    $(this).addClass("slideExpandUp");
+                }
+            });
+        });
